05315bb509b3e8dd6076e8271d93700b2f28cae0
noogen <tom@noogen.net>
2015-06-05 20:18:03 -0500

    update storeapp

--- asset/75/storeApp.js
+++ asset/75/storeApp.js
@@ -12,34 +12,34 @@
       gsn.applyConfig(gsn.config, true);
 
       var urls = [
-        { login: 0, store: 0, path: '/', tpl: gsn.getContentUrl('/views/home.html') }
-        , { login: 0, store: 0, path: '/article', tpl: gsn.getThemeUrl('/views/engine/article.html') }
-        , { login: 0, store: 0, path: '/article/:id', tpl: gsn.getThemeUrl('/views/engine/article.html') }
-        , { login: 0, store: 1, path: '/circular', tpl: gsn.getThemeUrl('/views/engine/circular-view.html')}
-        , { login: 0, store: 1, path: '/circular/:viewtype', tpl: gsn.getThemeUrl('/views/engine/circular-view.html') }
-        , { login: 0, store: 0, path: '/contactus', tpl: gsn.getThemeUrl('/views/engine/contact-us.html') }
-        , { login: 0, store: 1, path: '/coupons/digital', tpl: gsn.getThemeUrl('/views/engine/coupons-view.html') }
-        , { login: 0, store: 1, path: '/coupons/printable', tpl: gsn.getThemeUrl('/views/engine/coupons-view.html') }
-        , { login: 0, store: 1, path: '/coupons/store', tpl: gsn.getThemeUrl('/views/engine/coupons-view.html') }
-        , { login: 0, store: 0, path: '/mealplannerfull', tpl: gsn.getThemeUrl('/views/engine/meal-planner.html') }
-        , { login: 1, store: 0, path: '/savedlists', tpl: gsn.getThemeUrl('/views/engine/saved-lists.html') }
-        , { login: 0, store: 0, path: '/mylist', tpl: gsn.getThemeUrl('/views/engine/shopping-list.html') }
+        {   login: 0, store: 0, path: '/', tpl: gsn.getContentUrl('/views/home.html') }
+        , { login: 0, store: 0, layout: le3, path: '/article', tpl: gsn.getThemeUrl('/views/engine/article.html') }
+        , { login: 0, store: 0, layout: le3, path: '/article/:id', tpl: gsn.getThemeUrl('/views/engine/article.html') }
+        , { login: 0, store: 1, layout: le3, path: '/circular', tpl: gsn.getThemeUrl('/views/engine/circular-view.html')}
+        , { login: 0, store: 1, layout: le3, path: '/circular/:viewtype', tpl: gsn.getThemeUrl('/views/engine/circular-view.html') }
+        , { login: 0, store: 0, layout: le2, path: '/contactus', tpl: gsn.getThemeUrl('/views/engine/contact-us.html') }
+        , { login: 0, store: 1, layout: le3, path: '/coupons/digital', tpl: gsn.getThemeUrl('/views/engine/coupons-view.html') }
+        , { login: 0, store: 1, layout: le3, path: '/coupons/printable', tpl: gsn.getThemeUrl('/views/engine/coupons-view.html') }
+        , { login: 0, store: 1, layout: le3, path: '/coupons/store', tpl: gsn.getThemeUrl('/views/engine/coupons-view.html') }
+        , { login: 0, store: 0, layout: le3, path: '/mealplannerfull', tpl: gsn.getThemeUrl('/views/engine/meal-planner.html') }
+        , { login: 1, store: 0, layout: le3, path: '/savedlists', tpl: gsn.getThemeUrl('/views/engine/saved-lists.html') }
+        , { login: 0, store: 0, layout: le3, path: '/mylist', tpl: gsn.getThemeUrl('/views/engine/shopping-list.html') }
         , { login: 0, store: 0, path: '/mylist/print', tpl: gsn.getThemeUrl('/views/engine/shopping-list-print.html') }
-        , { login: 0, store: 0, path: '/mylist/email', tpl: gsn.getThemeUrl('/views/engine/shopping-list-email.html') }
-        , { login: 1, store: 0, path: '/myrecipes', tpl: gsn.getThemeUrl('/views/engine/my-recipes.html') }
+        , { login: 0, store: 0, layout: le2, path: '/mylist/email', tpl: gsn.getThemeUrl('/views/engine/shopping-list-email.html') }
+        , { login: 1, store: 0, layout: le3, path: '/myrecipes', tpl: gsn.getThemeUrl('/views/engine/my-recipes.html') }
         , { login: 1, store: 0, path: '/profile', tpl: gsn.getThemeUrl('/views/engine/profile-edit.html') }
-        , { login: 0, store: 0, path: '/recipe/search', tpl: gsn.getThemeUrl('/views/engine/recipe-search.html') }
-        , { login: 0, store: 0, path: '/recipe', tpl: gsn.getThemeUrl('/views/engine/recipe-details.html') }
-        , { login: 0, store: 0, path: '/recipe/:id', tpl: gsn.getThemeUrl('/views/engine/recipe-details.html') }
-        , { login: 0, store: 0, path: '/recipecenter', tpl: gsn.getThemeUrl('/views/engine/recipe-center.html') }
-        , { login: 0, store: 0, path: '/recipevideo', tpl: gsn.getThemeUrl('/views/engine/recipe-video.html') }
-        , { login: 0, store: 0, path: '/recipevideo/:id', tpl: gsn.getThemeUrl('/views/engine/recipe-video.html') }
+        , { login: 0, store: 0, layout: le3, path: '/recipe/search', tpl: gsn.getThemeUrl('/views/engine/recipe-search.html') }
+        , { login: 0, store: 0, layout: le2, path: '/recipe', tpl: gsn.getThemeUrl('/views/engine/recipe-details.html') }
+        , { login: 0, store: 0, layout: le2, path: '/recipe/:id', tpl: gsn.getThemeUrl('/views/engine/recipe-details.html') }
+        , { login: 0, store: 0, layout: le2, path: '/recipecenter', tpl: gsn.getThemeUrl('/views/engine/recipe-center.html') }
+        , { login: 0, store: 0, layout: le3, path: '/recipevideo', tpl: gsn.getThemeUrl('/views/engine/recipe-video.html') }
+        , { login: 0, store: 0, layout: le3, path: '/recipevideo/:id', tpl: gsn.getThemeUrl('/views/engine/recipe-video.html') }
         , { login: 0, store: 0, path: '/registration', tpl: gsn.getThemeUrl('/views/engine/registration.html') }
-        , { login: 0, store: 0, path: '/signin', tpl: gsn.getThemeUrl('/views/engine/signin.html') }
-        , { login: 0, store: 0, path: '/store/:id', tpl: gsn.getThemeUrl('/views/engine/store-info.html') }
-        , { login: 0, store: 0, path: '/storelocator', tpl: gsn.getThemeUrl('/views/engine/store-locator.html') }
-        , { login: 0, store: 0, path: '/unsubscribe', tpl: gsn.getThemeUrl('/views/engine/unsubscribe.html') }
-        , { login: 0, store: 0, path: '/blog', tpl: gsn.getContentUrl('/views/blog.html') }
+        , { login: 0, store: 0, layout: le3, path: '/signin', tpl: gsn.getThemeUrl('/views/engine/signin.html') }
+        , { login: 0, store: 0, layout: le1, path: '/store/:id', tpl: gsn.getThemeUrl('/views/engine/store-info.html') }
+        , { login: 0, store: 0, layout: le2, path: '/storelocator', tpl: gsn.getThemeUrl('/views/engine/store-locator.html') }
+        , { login: 0, store: 0, layout: le3, path: '/unsubscribe', tpl: gsn.getThemeUrl('/views/engine/unsubscribe.html') }
+        , { login: 0, store: 0, layout: le3, path: '/blog', tpl: gsn.getContentUrl('/views/blog.html') }
       ];
 
       angular.forEach(urls, function(v, k){
